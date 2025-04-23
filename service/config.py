f3f28e0fe547fdf1f895f8eac3595babdb511706
Каримзай Ахмад Халид <248499@edu.fa.ru>
2025-04-23 21:11:05 +0300

    dev: added logging in server

---
+++ service/config.py
@@ -0,0 +1,26 @@
+import os
+
+file_dir = os.path.dirname(os.path.abspath(__file__))
+server_log_file_path = os.path.join(file_dir, "logs", "server.log")
+
+log_config = {
+    "version": 1,
+    "formatters": {
+        "default": {
+            "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+        }
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "default",
+        },
+        "file": {
+            "class": "logging.FileHandler",
+            "filename": server_log_file_path,
+            "formatter": "default",
+        }
+    },
+    "root": {"level": "DEBUG", "handlers": ["console", "file"]},
+}
--- service/server.py
+++ service/server.py
@@ -1,24 +1,31 @@
 from flask import Flask, request, jsonify, render_template
+from logging.config import dictConfig
+import config as cfg
+
+dictConfig(cfg.log_config)
 
 app = Flask(__name__)
 
 @app.route('/api/numbers', methods=["POST"])
 def load_numbers():
-    data = request.get_json()
-
+    request_body = request.get_json()
+    app.debug.info(f"handling \"load_numbers\": request body: {request_body}")
     try:
-        nums = [int(data[f'num{i}']) for i in range(1, 5)]
+        nums = [int(request_body[f'num{i}']) for i in range(1, 5)]
         for num in nums:
             if num < 0:
                 raise ValueError("invalid entity")
     except (ValueError, KeyError):
+        app.logger.debug("Received invalid request body: %s", request_body)
         return jsonify({'error': 'Invalid input'}), 422
 
     result = sum(nums)
+    app.logger.debug(f"client response: {result}")
     return jsonify({'sum': result}), 200
 
 @app.route("/")
 def home():
+    app.logger.debug("server started")
     return render_template("index.html")
 
 if __name__ == '__main__':
